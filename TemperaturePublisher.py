627f679b131dd020dd545327ca2ac573cd02eb1c
Mesumaa <baechle-99@web.de>
2021-05-02 23:12:03 +0200

    changed sleeptime and adjusted loop

--- TemperaturePublisher.py
+++ TemperaturePublisher.py
@@ -2,17 +2,17 @@
 
 import rospy
 from sensor_msgs.msg import Temperature
-from time import time
+import time
 import RPi.GPIO as GPIO
 
-sleeptime = 1
+sleeptime = 10
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 base_dir = '/sys/bus/w1/devices/'
 
-device_folder = '28-3c01d60700ab'
+device_folder = base_dir + '28-3c01d60700ab'
 device_file = device_folder + '/w1_slave'
 
 def TemperaturMessung():
@@ -42,17 +42,19 @@ def talker():
    i = 0
    temperature = Temperature()
    rospy.init_node('Sensor', anonymous=True)
-   temperature.header.seq = i
-   temperature.header.stamp = rospy.Time.now()
-   temperature.header.frame_id = "Temperature Sensor"
 
-   temperature.temperature = TemperaturAuswertung()
+
+
    temperature.variance = None
    while not rospy.is_shutdown():
-
+        temperature.header.seq = i
+        temperature.header.stamp = rospy.Time.now()
+        temperature.header.frame_id = "Temperature Sensor"
+        temperature.temperature = TemperaturAuswertung()
         rospy.loginfo(temperature)
         pub.publish(temperature)
         time.sleep(sleeptime)
+        i = i + 1
 
 if __name__ == '__main__':
 
